557150bd12699c3f9d7e14b9c7e63b5b6145e00c
TasleemAhmad924 <tasleem@tuta.io>
2025-03-31 11:59:03 +0200

    feat: recreate datenschutz page in app directory and update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,6 +9,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
+    },
+    {
+      url: `${baseUrl}/datenschutz`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.3,
     }
   ]
 } 
