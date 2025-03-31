83f7495f1d5833253f1e72084fb0bc8846936ae6
TasleemAhmad924 <tasleem@tuta.io>
2025-03-31 04:18:07 +0200

    feat: update metadata, favicon config, and add site manifest

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.josef-resch.de'
+  const baseUrl = 'https://www.gefahr-ist-mein-beruf.com'
   
   return [
     {
